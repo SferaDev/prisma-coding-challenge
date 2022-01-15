07bf40411c33ee88d71b00c0a0a93cf634f8bf27
Alexis Rico <sferadev@gmail.com>
2022-01-15 17:29:40 +0100

    chore: Seed admin user
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 const db = new PrismaClient();
 
-async function seed() {}
+async function seed() {
+    await db.user.create({
+        data: { email: "admin@local", name: "Admin", apiToken: "161a8803-c0d8-4164-a70d-990e33bb550a" },
+    });
+}
 
 seed();
