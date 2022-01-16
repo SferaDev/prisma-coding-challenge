c02081818aef4a10f330457e0a77c40b636f1560
Alexis Rico <sferadev@gmail.com>
2022-01-16 12:04:45 +0100

    feat(auth): Redirect on endpoint
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- app/routes/auth.github.tsx
+++ app/routes/auth.github.tsx
@@ -1,5 +1,7 @@
-import type { ActionFunction } from "remix";
+import { ActionFunction, LoaderFunction, redirect } from "remix";
 import { authenticator } from "~/services/auth.server";
+
+export const loader: LoaderFunction = () => redirect("/login");
 
 export const action: ActionFunction = async ({ request }) => {
     return await authenticator.authenticate("github", request, {
